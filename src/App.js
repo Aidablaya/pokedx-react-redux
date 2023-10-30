4b7d597135a444a68267225966899334f9da40a7
Aidablaya <you@example.com>
2023-10-30 15:19:19 +0100

    add tailwind

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Col, Spin } from 'antd';
 import Searcher from './components/Searcher';
 import PokemonList from './components/PokemonList';
-import logo from './statics/logo.svg';
+import logo from './statics/pokemon.jpg';
 import './App.css';
 import { useEffect } from 'react';
 import { shallowEqual, useDispatch, useSelector } from 'react-redux';
@@ -20,13 +20,13 @@ function App() {
   },[dispatch])
 
   return (
-    <div className="App">
-      <Col span={4} offset={10}>
-        <img src={logo} alt="Pokedux" />
-      </Col>
-      <Col span={8} offset={8}>
+    <div className="bg-black">
+      <header className='m-6'>
+        <img src={logo} alt="Pokedux" className='m-6' />
+      </header>
+      <main >
         <Searcher />
-      </Col>
+      </main>
       {loading ? <Col  offset={12}>
         <Spin spinning size='large'/>
       </Col> : <PokemonList pokemons={pokemons}/>}
--- src/components/Searcher.jsx
+++ src/components/Searcher.jsx
@@ -1,7 +1,24 @@
-import {Input} from 'antd';
+
+import { Search } from 'react-feather';
+
 
 const Searcher = () => {
-    return <Input.Search placeholder='Buscar....' style={{ marginBottom: 10 }} />;
+  
+
+  return (
+    <div className="flex items-center justify-center">
+      <div className="relative">
+        <div className="absolute inset-0 left-3 top-1/2 transform -translate-y-1/2 pointer-events-none">
+          <Search size={20} />
+        </div>
+        <input
+          type="text"
+          className="flex items-center mb-3 p-2 pl-10 pr-3 border-2 border-black-500 w-64 rounded-lg"
+          placeholder="Buscar Pokemon"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Searcher;
